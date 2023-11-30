4b2119c2ccc59ddbc698c9b0fd7f7c006af5762e
ikerlunaas <124459129+ikerlunaas@users.noreply.github.com>
2023-11-30 21:02:12 +0000

    Add files and folders

---
+++ code/AdditionOfNums/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float n1, n2, sum;
+
+    cout<<"Insert the first number: ";
+    cin >> n1;
+
+    cout<<"Insert the second number: ";
+    cin >> n2;
+
+    sum = n1 + n2;
+
+    cout<<"The adittion of the numbers is: "<<sum<<endl;
+
+
+    return 0;
+}
